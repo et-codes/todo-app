b8939c05fe6eefcfe8746beb1eac3b73fa20e890
Eric Thornton <ericthornton43@gmail.com>
2022-08-25 19:59:36 -0600

    Use form submit instead of click.

--- script.js
+++ script.js
@@ -2,17 +2,22 @@ let nextId = 0;
 
 const addTodo = (event) => {
   // Append todo item onto the todo list
+  event.preventDefault();
+
   const todoTextInput = document.getElementById('todo-input');
   const todoText = todoTextInput.value;
   todoTextInput.value = '';
-  updateTodoList(todoText);
+
+  const divTodoList = document.getElementsByClassName('todo-list')[0];
+  const divTodoItem = createTodo(todoText);
+  divTodoList.appendChild(divTodoItem);
 }
 
 const toggleTodoComplete = (event) => {
   // Change font to strikethrough if checkbox is checked, and vice versa
   const checkbox = event.target;
-  const idNum = checkbox.id.split('-')[1];
-  const label = document.getElementById(`todo-${idNum}`);
+  const index = checkbox.id.split('-')[1];
+  const label = document.getElementById(`todo-${index}`);
 
   if (checkbox.checked) {
     label.style.textDecoration = 'line-through';
@@ -23,15 +28,18 @@ const toggleTodoComplete = (event) => {
 
 const removeTodo = (event) => {
   // Remove item from todo list
+  const index = event.target.id.split('-')[1];
+  document.getElementById(`todo-item-${index}`).remove();
 }
 
 const createTodo = (todoText) => {
   // Create the div that will display each todo list item
-  const divTodoItem = document.createElement('div');
-  divTodoItem.classList.add('todo-item');
-
   const id = nextId;
   nextId++;
+
+  const divTodoItem = document.createElement('div');
+  divTodoItem.classList.add('todo-item');
+  divTodoItem.id = `todo-item-${id}`;
 
   const checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
@@ -50,17 +58,11 @@ const createTodo = (todoText) => {
   img.src = 'remove.png';
   img.alt = 'remove button';
   img.id = `remove-${id}`;
+  img.addEventListener('click', removeTodo);
   divTodoItem.appendChild(img);
 
   return divTodoItem;
 }
 
-const updateTodoList = (todoText) => {
-  // Generate the todo list to be displayed on the screen
-  const divTodoList = document.getElementsByClassName('todo-list')[0];
-  const divTodoItem = createTodo(todoText);
-  divTodoList.appendChild(divTodoItem);
-}
-
-const todoButton = document.getElementById('todo-submit');
-todoButton.addEventListener('click', addTodo);
+const todoForm = document.getElementById('todo-form');
+todoForm.addEventListener('submit', addTodo);

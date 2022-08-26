22b78eace543ab1d36d8f0bee0d755082207dc18
Eric Thornton <ericthornton43@gmail.com>
2022-08-26 10:22:37 -0600

    Refactored to not use id's.

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-let nextId = 0;
-
 const addTodo = (event) => {
   // Append todo item onto the todo list
   event.preventDefault();
 
-  const todoTextInput = document.getElementById('todo-input');
+  const todoTextInput = event.target.getElementsByClassName('text-input')[0];
   const todoText = todoTextInput.value;
   todoTextInput.value = '';
 
@@ -16,53 +14,39 @@ const addTodo = (event) => {
 const toggleTodoComplete = (event) => {
   // Change font to strikethrough if checkbox is checked, and vice versa
   const checkbox = event.target;
-  const index = checkbox.id.split('-')[1];
-  const label = document.getElementById(`todo-${index}`);
-
-  if (checkbox.checked) {
-    label.style.textDecoration = 'line-through';
-  } else {
-    label.style.textDecoration = 'none';
-  }
+  const label = checkbox.parentElement;
+  label.style.textDecoration = checkbox.checked ? 'line-through' : 'none';
 }
 
 const removeTodo = (event) => {
   // Remove item from todo list
-  const index = event.target.id.split('-')[1];
-  document.getElementById(`todo-item-${index}`).remove();
+  event.target.parentElement.remove();
 }
 
 const createTodo = (todoText) => {
   // Create the div that will display each todo list item
-  const id = nextId;
-  nextId++;
-
   const divTodoItem = document.createElement('div');
   divTodoItem.classList.add('todo-item');
-  divTodoItem.id = `todo-item-${id}`;
 
   const checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
   checkbox.classList.add('boxes');
-  checkbox.id = `box-${id}`;
   checkbox.addEventListener('change', toggleTodoComplete);
-  divTodoItem.appendChild(checkbox);
 
   const label = document.createElement('label');
-  label.htmlFor = checkbox.id;
-  label.id = `todo-${id}`;
-  label.textContent = todoText;
+  const labelText = document.createTextNode(todoText);
+  label.appendChild(checkbox);
+  label.appendChild(labelText);
   divTodoItem.appendChild(label);
 
   const img = document.createElement('img');
   img.src = 'remove.png';
   img.alt = 'remove button';
-  img.id = `remove-${id}`;
   img.addEventListener('click', removeTodo);
   divTodoItem.appendChild(img);
 
   return divTodoItem;
 }
 
-const todoForm = document.getElementById('todo-form');
-todoForm.addEventListener('submit', addTodo);
+document.getElementsByClassName('todo-form')[0]
+  .addEventListener('submit', addTodo);
